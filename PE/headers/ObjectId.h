1142dd11b937b891466cf05c59c030710d552a2d
Dawid Pura <puradawid@gmail.com>
2013-12-26 16:42:59 +0100

    Add parametrized and non-parametrized constructors to ObjectId class

--- PE/headers/ObjectId.h
+++ PE/headers/ObjectId.h
@@ -1,9 +1,20 @@
 #ifndef OBJECTID_H
 #define OBJECTID_H
 
+#include <Gl\gl.h>
+
 struct ObjectId
 {
 	GLint id;
+	ObjectId(int id)
+	{
+		this->id = id;
+	}
+
+	ObjectId()
+	{
+		this->id = 0;
+	}
 } typedef ObjectId;
 
 #endif
