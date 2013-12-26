212861af770c6a36d0750f87b277c48f543e7461
Dawid Pura <puradawid@gmail.com>
2013-12-26 16:38:48 +0100

    Virtual method getMesh has to be defined

--- PE/headers/MeshBuildStrategy.h
+++ PE/headers/MeshBuildStrategy.h
@@ -1,10 +1,15 @@
 #ifndef MESHBUILDSTRATEGY_H
 #define MESHBUILDSTRATEGY_H
 
+#include "Mesh.h"
+#include "Point.h"
+
+#include <vector>
+
 class MeshBuildStrategy
 {
 public:
-	virtual Mesh* getMesh(std::vector<std::vector<Point*>*>);
+	virtual Mesh* getMesh(std::vector<std::vector<Point>*>*) = 0;
 };
 
 #endif
