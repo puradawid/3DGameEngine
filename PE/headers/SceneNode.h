838b14407c8779b3c74cbb08b8611aeab1cdba36
Dawid Pura <puradawid@gmail.com>
2013-12-26 16:45:22 +0100

    Resolve including problem, add static counter field, childs list and id member

--- PE/headers/SceneNode.h
+++ PE/headers/SceneNode.h
@@ -1,11 +1,17 @@
 #ifndef SCENENODE_H
 #define SCENENODE_H
 
+#include <vector>
+
 #include "ObjectId.h"
 #include "UpdateClues.h"
+#include "RenderClues.h"
 
 class SceneNode
 {
+	static int counter;
+	std::vector<SceneNode*> childs;
+	ObjectId id;
 public:
 	ObjectId addNode(SceneNode*);
 	void removeNode(ObjectId);
