52659a9b8289add65f986e15a3ab1257ce70a4e4
Dawid Pura <puradawid@gmail.com>
2013-12-26 16:46:05 +0100

    Declare constructors and add update method

--- PE/headers/StaticObject.h
+++ PE/headers/StaticObject.h
@@ -1,11 +1,18 @@
 #ifndef STATICOBJECT_H
 #define STATICOBJECT_H
 
+#include "SceneNode.h"
+#include "Mesh.h"
+
 class StaticObject : public SceneNode
 {
 	Mesh* mesh;
 public:
+	StaticObject(Mesh*);
+	StaticObject();
+
 	virtual void render(RenderClues*);
+	virtual void update(UpdateClues*);
 };
 
 #endif
