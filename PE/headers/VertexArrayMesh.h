2661c06e3efb7104e3c81aee58f50ea6ea4d968b
Dawid Pura <puradawid@gmail.com>
2013-12-26 16:47:55 +0100

    Repiar includes and add implementation methods

--- PE/headers/VertexArrayMesh.h
+++ PE/headers/VertexArrayMesh.h
@@ -1,11 +1,21 @@
 #ifndef VERTEXARRAYMESH_H
 #define VERTEXARRAYMESH_H
 
-class VertexArrayMesh
+#include "Mesh.h"
+#include "Point.h"
+
+#include <GL/gl.h>
+#include <vector>
+
+class VertexArrayMesh : public Mesh
 {
+	friend class VertexArrayBuildStrategy;
 	GLint id;
+	std::vector<std::vector<Point>*>* mesh;
+	void setMesh(std::vector<std::vector<Point>*>*);
 public:
-	virtual void render(RenderClues*);
+	virtual void render(RenderClues*); //rendering mesh
+	VertexArrayMesh();
 };
 
 #endif
