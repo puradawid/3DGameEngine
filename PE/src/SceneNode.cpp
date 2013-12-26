61fda4229c2595beab15dd209a548426420be64a
Dawid Pura <puradawid@gmail.com>
2013-12-26 16:49:10 +0100

    Add rendering implementation (for all childs)

---
+++ PE/src/SceneNode.cpp
@@ -0,0 +1,26 @@
+#include "../headers/SceneNode.h"
+
+ObjectId SceneNode::addNode(SceneNode* child)
+{
+	this->childs.push_back(child);
+	ObjectId obj(this->childs.size() - 1);
+	return ObjectId(obj);
+}
+
+void SceneNode::removeNode(ObjectId id)
+{
+	childs.erase(childs.begin() + id.id);
+}
+
+void SceneNode::render(RenderClues* rc)
+{
+	for(auto node : childs)
+	{
+		node->render(rc);
+	}
+}
+
+void SceneNode::update(UpdateClues* uc)
+{
+
+}
