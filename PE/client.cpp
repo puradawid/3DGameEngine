13f0d82d26b5c2c340d50410afa98d98af97e47c
Dawid Pura <puradawid@gmail.com>
2013-12-26 09:10:11 +0100

    Example usage of timers!

--- PE/client.cpp
+++ PE/client.cpp
@@ -1,4 +1,22 @@
 #include "./headers/Game.h"
+#include "./headers/Timer.h"
+
+#include <stdio.h>
+
+class MyTimer : public Timer
+{
+public:
+	MyTimer(int ticks, int interval)
+	: Timer(ticks, interval)
+	{
+
+	}
+
+	virtual void tick()
+	{
+		printf("tick!\n");
+	}
+};
 
 class MyGame : public Game
 {
@@ -6,6 +24,7 @@ class MyGame : public Game
 public:
 	virtual void initializeGame(GameConfig* config)
 	{
+		this->getTimerClock()->addTimer(new MyTimer(1000, 1000));
 	}
 };
 int main()
