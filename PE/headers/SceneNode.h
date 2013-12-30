2b28a3ab7fe90720a462b65f430409177149fa3b
Dawid Pura <puradawid@gmail.com>
2013-12-30 06:57:21 +0100

    Adding method to API - setRenderStrategy for all nodes - even it is not used

--- PE/headers/SceneNode.h
+++ PE/headers/SceneNode.h
@@ -7,15 +7,20 @@
 #include "UpdateClues.h"
 #include "RenderClues.h"
 #include "SimplePoint.h"
+#include "RenderStrategy.h"
 class SceneNode
 {
 	static int counter;
 	std::vector<SceneNode*> childs;
 	SimplePoint rotation, translation, scale;
 	ObjectId id;
+protected:
+	RenderStrategy* rs;
 public:
 	ObjectId addNode(SceneNode*);
 	void removeNode(ObjectId);
+	void setRenderStrategy(RenderStrategy*);
+
 	virtual void render(RenderClues*);
 	virtual void update(UpdateClues*);
 };
