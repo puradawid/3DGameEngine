70d5d1d16c85f03396e54b9fcd65b017288e4100
Dawid Pura <puradawid@gmail.com>
2014-01-04 19:53:01 +0100

    Implements updating camera node and updateWolrd method

--- PE/src/Camera.cpp
+++ PE/src/Camera.cpp
@@ -1,10 +1,24 @@
 #include "../headers/Camera.h"
 
+#ifdef _MSC_VER
+	#include <Windows.h>
+#endif
+	#include <GL/gl.h>
+
 void Camera::update(UpdateClues* uc)
 {
-
+	SceneNode::update(uc);
+	currentState.p = getTranslation();
+	currentState.rot = getRotation();
 }
 
 Camera::~Camera(){
 
+}
+
+void Camera::updateWorld()
+{
+	glRotatef(-currentState.rot.x, 1.0, 0, 0);
+    glRotatef(-currentState.rot.y, 0, 1.0, 0);
+    glTranslatef(-currentState.p.x, -currentState.p.y, -currentState.p.z); //can be reverted moves
 }
