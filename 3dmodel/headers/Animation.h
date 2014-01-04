cfda3e5dc7cec69e85dd2e4111b6717c94d39b75
Dawid Pura <puradawid@gmail.com>
2014-01-04 13:48:12 +0100

    Added frames containter, default contstructor, assigning node and setting new frame functionallities

--- 3dmodel/headers/Animation.h
+++ 3dmodel/headers/Animation.h
@@ -2,10 +2,20 @@
 #define ANIMATION_H
 
 #include "TreeNode.h"
+#include "Frame.h"
+
+#include <vector>
+
 
 class Animation
 {
+protected:
 	TreeNode* object;
+	std::vector<Frame*> frames;
+public:
+	Animation() { object = NULL; }
+	void assignNode(TreeNode* object)	{ this->object = object; }
+	void setNextFrame(Frame* frame)		{ this->frames.push_back(frame); }
 };
 
 #endif
