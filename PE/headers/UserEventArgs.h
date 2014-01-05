9f5194bf2221855732327bad188e33e4060b1d27
Dawid Pura <puradawid@gmail.com>
2014-01-05 13:20:31 +0100

    Changes InputType to enumerator - without other parameters

--- PE/headers/UserEventArgs.h
+++ PE/headers/UserEventArgs.h
@@ -1,16 +1,28 @@
 #ifndef USEREVENTARGS_H
 #define USEREVENTARGS_H
 
-struct InputType
+enum InputType
 {
-	enum {Keyboard, Mouse} type;
+	SpecialKeyboard, Keyboard, Mouse
 }typedef InputType;
 
 class UserEventArgs
 {
+protected:
 	InputType type;
+	char event;
+public:
 	bool isKeyboard();
 	bool isMouse();
+	UserEventArgs()
+	{
+		this->type = InputType();
+	}
+	UserEventArgs(InputType type, char event)
+	{
+		this->type = type;
+		this->event = event;
+	}
 };
 
 #endif
