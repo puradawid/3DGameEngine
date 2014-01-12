86adcf44b154c51d3a01ce9fc0c8737a4d688c7d
Dawid Pura <puradawid@gmail.com>
2014-01-12 13:35:46 +0100

    Added getSize and getPoint methods to API

--- PE/headers/Mesh.h
+++ PE/headers/Mesh.h
@@ -2,12 +2,15 @@
 #define MESH_H
 
 #include "RenderClues.h"
+#include "Point.h"
 
 class Mesh
 {
 public:
 	virtual void render(RenderClues*) {}; //print mesh on scene
 	virtual ~Mesh() {};
+	virtual Point getPoint(int index) {};
+	virtual int getSize() {};
 };
 
 #endif
