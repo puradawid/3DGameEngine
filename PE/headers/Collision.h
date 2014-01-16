a3fed125087837b9e932c86cc8ee73bb3a386b33
Dawid Pura <puradawid@gmail.com>
2014-01-16 13:25:15 +0100

    Extend collision parameters

--- PE/headers/Collision.h
+++ PE/headers/Collision.h
@@ -1,11 +1,29 @@
 #ifndef COLLISION_H
 #define COLLISION_H
+
 #include <vector>
+
+#include "SimplePoint.h"
 #include "SceneNode.h"
 
 class Collision
 {
+	SceneNode* reason;
 	std::vector<SceneNode*> objects;
+	SimplePoint vector;
+	TransformationType type;
+public:
+	SceneNode* getReason(){return reason;}
+	std::vector<SceneNode*> getObjects() {return objects;};
+	SimplePoint getVector() {return vector;}
+	TransformationType getType() {return type;}
+	Collision(SceneNode* reason, std::vector<SceneNode*> objects, SimplePoint vector, TransformationType type)
+	{
+		this->reason = reason;
+		this->objects = objects;
+		this->vector = vector;
+		this->type = type;
+	}
 };
 
 #endif
