869ee706083dcdd49cfa470de31da4463d119be8
Dawid Pura <puradawid@gmail.com>
2014-01-16 13:43:18 +0100

    Changed detection of collision

--- PE/headers/CollisionDetector.h
+++ PE/headers/CollisionDetector.h
@@ -5,14 +5,22 @@
 
 #include "CollisionObserver.h"
 #include "Collision.h"
+#include "BoundingBox.h"
+#include "SimplePoint.h"
+#include "SceneNode.h"
 
 class CollisionDetector
 {
+	friend class SceneNode;
 	std::vector<CollisionObserver*> observers;
+	std::vector<SceneNode*> collisionables;
+	void notifyDetector(SceneNode*, TransformationType, SimplePoint);
 public:
 	CollisionDetector();
 	void addObserver(CollisionObserver*);
 	void dismissObserver(CollisionObserver*);
+	void addCollisionable(SceneNode*);
+	void dismissCollisionable(SceneNode*);
 	void notifyObservers(Collision*);
 };
 
