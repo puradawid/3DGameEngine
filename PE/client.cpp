e90d1b4ba2f450d90d3ef4c41948d2e5ce8b19de
Dawid Pura <puradawid@gmail.com>
2014-01-21 17:00:54 +0100

    Client app - flying and killing by objects

--- PE/client.cpp
+++ PE/client.cpp
@@ -10,6 +10,7 @@
 #include "./headers/UserEventObserver.h"
 #include "./headers/UserEventArgs.h"
 #include "./headers/CollisionObserver.h"
+#include "./headers/PECommand.h"
 
 #include <stdio.h>
 #include <GL/glut.h>
@@ -29,7 +30,7 @@ public:
 
 	virtual void tick()
 	{
-		//rocket->move(SimplePoint(-0.1,0,0));
+		rocket->move(SimplePoint(0,0,-0.15));
 	}
 };
 
@@ -48,54 +49,66 @@ class CollisionObserverNotify : public CollisionObserver
 	}
 };
 
+class KillCommand : public PECommand
+{
+public:
+	virtual void doCommand()
+	{
+		rocket->move(SimplePoint(0,0,20));
+	}
+};
+
+class CollisionObserverKiller : public CollisionObserver
+{
+	std::vector<SceneNode*> killers;
+	PECommand* killCommand;
+public:
+
+	CollisionObserverKiller(PECommand* kill)
+	{
+		this->killCommand = kill;
+	}
+
+	void addKiller(SceneNode* killer)
+	{
+		killers.push_back(killer);
+	}
+
+	virtual void collisionDetected(Collision* collision)
+	{
+		SceneNode* rocket = collision->getReason();
+		SceneNode* killer = collision->getObjects()[0];
+		for(int i=0; i < killers.size(); i++)
+			if(killer == killers[i])
+				this->killCommand->doCommand();
+	}
+};
+
 class MovementObserver : public UserEventObserver
 {
 public:
+
+	const double jump = 0.5;
+
 	virtual void handleEvent(UserEventArgs* arg)
 	{
 		if(arg->isSpecialKeyboard() && arg->getEvent() == GLUT_KEY_LEFT)
 		{
-			rocket->move(SimplePoint(0.1,0,0));
+			rocket->move(SimplePoint(jump, 0, 0));
 		}
 
 		if(arg->isSpecialKeyboard() && arg->getEvent() == GLUT_KEY_RIGHT)
 		{
-			rocket->move(SimplePoint(-0.1,0,0));
+			rocket->move(SimplePoint(-jump, 0, 0));
 		}
-
 		if(arg->isSpecialKeyboard() && arg->getEvent() == GLUT_KEY_UP)
 		{
-			rocket->move(SimplePoint(0,0,-0.1));
+			rocket->move(SimplePoint(0, jump, 0));
 		}
 
 		if(arg->isSpecialKeyboard() && arg->getEvent() == GLUT_KEY_DOWN)
 		{
-			rocket->move(SimplePoint(0,0,0.1));
-		}
-		if(arg->isSpecialKeyboard() && arg->getEvent() == GLUT_KEY_HOME)
-		{
-			helper->rotate(SimplePoint(0.1,0,0));
-		}
-		if(arg->isSpecialKeyboard() && arg->getEvent() == GLUT_KEY_END)
-		{
-			helper->rotate(SimplePoint(0,0.1,0));
-		}
-
-		if(arg->isKeyboard() && arg->getEvent() == 's')
-		{
-			helper->move(SimplePoint(0,-0.1,0));
-		}
-		if(arg->isKeyboard() && arg->getEvent() == 'w')
-		{
-			helper->move(SimplePoint(0,0.1,0));
-		}
-		if(arg->isKeyboard() && arg->getEvent() == 'a')
-		{
-			helper->rotate(SimplePoint(0,0.1,0));
-		}
-		if(arg->isKeyboard() && arg->getEvent() == 'a')
-		{
-			helper->rotate(SimplePoint(0,-0.1,0));
+			rocket->move(SimplePoint(0, -jump, 0));
 		}
 	}
 };
@@ -113,6 +126,48 @@ class MyGame : public Game
 		return sn;
 	}
 
+	void generateBoxLine(SimplePoint begin, SimplePoint end)
+	{
+		double value = 0.0;
+		if(begin.x - end.x == 0 && begin.z - end.z == 0)
+		{
+			for(int i = 0; value + 1 <= end.y - begin.y; i++ )
+				generateBox(begin.x, begin.y + value++, begin.z);
+		} else
+		if(begin.y - end.y == 0 && begin.z - end.z == 0)
+		{
+			for(int i = 0; value + 1 <= end.x - begin.x; i++ )
+				generateBox(begin.x + value++, begin.y, begin.z);
+		}
+		if(begin.x - end.x == 0 && begin.z - end.z == 0)
+		{
+			for(int i = 0; value + 1 >= end.y - begin.y; i++ )
+				generateBox(begin.x, begin.y + value--, begin.z);
+		} else
+		if(begin.y - end.y == 0 && begin.z - end.z == 0)
+		{
+			for(int i = 0; value + 1 >= end.x - begin.x; i++ )
+				generateBox(begin.x + value--, begin.y, begin.z);
+		}
+	}
+	void generateGate(SimplePoint center)
+	{
+		double width = 20, height = 20;
+		SimplePoint a(center.x - width/2, center.y - width/2, center.z);
+		generateBoxLine(SimplePoint(a.x,a.y,a.z), SimplePoint(a.x + width, a.y, a.z));
+		generateBoxLine(SimplePoint(a.x + width, a.y, a.z), SimplePoint(a.x + width, a.y + height, a.z));
+		generateBoxLine(SimplePoint(a.x + width, a.y + height, a.z), SimplePoint(a.x, a.y + height, a.z));
+		generateBoxLine(SimplePoint(a.x, a.y + height, a.z), SimplePoint(a.x, a.y, a.z));
+	}
+
+	SceneNode* loadObject(std::string resource)
+	{
+		StaticObject* sn = new StaticObject(this->getOBJPool()->getMesh(mbs, resource));
+		sn->setRenderStrategy(dynamic_cast<RenderStrategy*>(new MaterialRenderStrategy()));
+		this->getScene()->getRoot()->addNode(sn);
+		return sn;
+	}
+
 public:
 
 	virtual void initializeGame(GameConfig* config)
@@ -121,47 +176,34 @@ public:
 		mbs = dynamic_cast<MeshBuildStrategy*>(new VertexArrayBuildStrategy());
 		getUserEventManager()->addObserver(new MovementObserver());
 
-		StaticObject* sn = new StaticObject(this->getOBJPool()->getMesh(mbs, "./resources/obj/tower.obj"));
-		sn->setRenderStrategy(dynamic_cast<RenderStrategy*>(new MaterialRenderStrategy()));
-		sn->move(SimplePoint(4,0,0));
-		this->getScene()->getRoot()->addNode(sn);
-		
-		StaticObject* s2 = new StaticObject(this->getOBJPool()->getMesh(mbs, "./resources/obj/tower.obj"));
-		s2->setRenderStrategy(dynamic_cast<RenderStrategy*>(new MaterialRenderStrategy()));
-		s2->move(SimplePoint(7,0,0));
-		this->getScene()->getRoot()->addNode(s2);
-
 		StaticObject* s1 = new StaticObject(this->getOBJPool()->getMesh(mbs, "./resources/obj/rocket.obj"));
 		s1->setRenderStrategy(dynamic_cast<RenderStrategy*>(new MaterialRenderStrategy()));
 		this->getScene()->getRoot()->addNode(s1);
-		s1->move(SimplePoint(10,10,0));
+		s1->move(SimplePoint(10,10,100));
 		s1->rotate(SimplePoint(0,90,0));
 		rocket = dynamic_cast<SceneNode*>(s1);
-		helper = s1;
+
+		loadObject("./resources/obj/tunnel.obj")->rotate(SimplePoint(0,90,0));
 
 		camera = new Camera();
 		this->getScene()->setCamera(camera);
 		rocket->addNode(camera);
-		camera->move(SimplePoint(0,7,14));
+		camera->move(SimplePoint(0,12,14));
 		camera->rotate(SimplePoint(0,-90,0));
 		camera->rotate(SimplePoint(-15, 0,0));
 
-		StaticObject* s3 = new StaticObject(this->getOBJPool()->getMesh(mbs, "./resources/obj/cube.obj"));
-		s3->setRenderStrategy(dynamic_cast<RenderStrategy*>(new MaterialRenderStrategy()));
-		this->getScene()->getRoot()->addNode(s3);
-		s3->move(SimplePoint(0,4,0));
 		getCollisionDetector()->addObserver(dynamic_cast<CollisionObserver*>(new CollisionObserverNotify()));
+		CollisionObserverKiller* cok = new CollisionObserverKiller(new KillCommand());
+		getCollisionDetector()->addObserver(dynamic_cast<CollisionObserver*>(cok));
+
 		getCollisionDetector()->addCollisionable(s1);
-		getCollisionDetector()->addCollisionable(sn);
-		getCollisionDetector()->addCollisionable(s2);
-		getCollisionDetector()->addCollisionable(s3);
 
-		for(int i = 0; i < 100; i++)
+		for(int i = 0; i < 20; i++)
 		{
-			//getCollisionDetector()->addCollisionable(generateBox(i*2,i*2,i*2));
-			generateBox(i*2,i*2,i*2);
+			SceneNode* box = generateBox(0, 20, -i*20);
+			cok->addKiller(box);
+			getCollisionDetector()->addCollisionable(box);
 		}
-
 		rocket->update(NULL);
 	}
 };
